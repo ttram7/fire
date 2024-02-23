065d4b4e7184368182d571cf18e701e0abf18781
mandi andrade <mandi.andrade20@gmail.com>
2024-02-23 16:49:05 -0600

    initialized home components and assets to show search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+  ],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'fire-app';
 }
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+ import { CommonModule } from '@angular/common';
+
+ @Component({
+   selector: 'app-housing-location',
+   standalone: true,
+   imports: [CommonModule],
+   template: `
+     <p>
+       housing-location works!
+     </p>
+   `,
+   styleUrl: './housing-location.component.css'
+ })
+ export class HousingLocationComponent {
+
+ }
