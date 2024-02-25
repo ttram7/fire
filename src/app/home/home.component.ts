2cc14a0e70267fc35bf886be85b74ceee88cfe48
Tina Tram <tinatram07@gmail.com>
2024-02-24 19:00:35 -0600

    changed any reference of scss to css

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation'; // use HousingLocation interface
+import { HousingService } from '../housing.service';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+    
+    <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation">
+      
+    </app-housing-location>
+  </section>
+  `,
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+constructor() {
+  this.housingLocationList = this.housingService.getAllHousingLocations();
+}
+}
+  // housingLocation property of type HousingLocation to HomeComponent class
+  // inject new service, that gets data from static array, into HomeComponent class 
+  // constructor is first function to run when component is created
+
+
