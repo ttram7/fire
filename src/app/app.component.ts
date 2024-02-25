8b99e3dc689b854ab8c6f7f0b7d206ee7ea2b885
mandi andrade <mandi.andrade20@gmail.com>
2024-02-25 12:37:42 -0600

    removed duplicate import after merge conflicts were resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
