2a67249e6c9f46f14c5a66c24f1e0b10f71446c7
Tina Tram <tinatram07@gmail.com>
2024-02-26 19:50:19 -0600

    display id for housing location

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
@@ -6,11 +10,16 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <p>
-      details works!
+      details works!{{housingLocationId}}
     </p>
   `,
   styleUrl: './details.component.css'
 })
-export class DetailsComponent {
 
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
