15953f00c91e2d80dd375b38ced92d8b4a5bfd86
Tina Tram <tinatram07@gmail.com>
2024-02-26 19:21:27 -0600

    created details component

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      details works!
+    </p>
+  `,
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+
+}
