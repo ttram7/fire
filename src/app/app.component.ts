43046ae71162f2fb44e95add32751216d80a7593
Tina Tram <tinatram07@gmail.com>
2024-02-26 19:45:23 -0600

    created route to details page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
