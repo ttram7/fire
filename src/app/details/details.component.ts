d15cdc883d24504f410e1c35f05efb71000b9f5a
Daniel Pathammavong <patha037@umn.edu>
2024-03-13 19:06:46 -0500

    Changed photo size and position. And moved Location name. And Changed section heading font size, and added border to quick flame

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -10,14 +10,14 @@ import { HousingLocation } from '../housinglocation';
   imports: [],
   template: `
   <article>
+  <h2 class="listing-heading">{{housingLocation?.name}}</h2>
     <img class="listing-photo" [src]="housingLocation?.photo"
       alt="Exterior photo of {{housingLocation?.name}}"/>
     <section class="listing-description">
-      <h2 class="listing-heading">{{housingLocation?.name}}</h2>
       <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
     </section>
     <section class="listing-features">
-      <h2 class="section-heading">About this housing location</h2>
+      <h2 class="section-heading">Quick Flame</h2>
       <ul>
         <li>Units available: {{housingLocation?.availableUnits}}</li>
         <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
