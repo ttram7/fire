f7080e7b1d1729f25a05d98600efb2f11fe217e6
Daniel Pathammavong <patha037@umn.edu>
2024-03-20 15:24:43 -0500

    added another column with flexbox, link to zillow homes, and summary section

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -10,17 +10,18 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   standalone: true,
   imports: [FlexLayoutModule],
   template: `
-  <article fxLayout= "column">
-      <section fxLayout= "column" fxLayoutAlign="right" >
-</section>
-
+  
+  <article fxLayout= "row">
+  
+  <main fxLayout= "column"fxFlex="80%">
+    
   <h2 class="item item-1" class="listing-heading">{{housingLocation?.name}}</h2>
     <img class="item item-2" class="listing-photo" [src]="housingLocation?.photo"
       alt="Exterior photo of {{housingLocation?.name}}"/>
     <section class="listing-description">
-      <p fxLayoutAlign="center center"class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      <p fxLayoutAlign="center center" class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
     </section>
-    <section class="item item-3 listing-features" fxFlex="10%" >
+    <section class="item item-3 listing-features" fxFlex="25%" >
       <div>
       <h2 fxLayoutAlign="center center" class="section-heading">Quick Flame</h2>
       </div>
@@ -31,19 +32,39 @@ import { FlexLayoutModule } from '@angular/flex-layout';
         <li class="item item-3">Population: {{housingLocation?.laundry}}</li>
       </ul>
 </div>
-
  </section>
+</main>
 
 
 
- <section>
-  <h2> Cost of Living </h2>
-  <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-</section>
+ <aside class="item item-1" fxFlex="20%">
+  <h2> Friendly Fire Grade </h2>
+
+ <a href="https://www.zillow.com/homes/{{housingLocation?.city}}_rb">Homes For Sale ></a>
+
+</aside>
+
+
 
 
     
   </article>
+
+
+
+  <summary>
+  
+<h1>Cost of Living</h1>
+<p>   This will be data + details from the database TBD  </p>
+
+<h1>Cost of Living</h1>
+<p>   This will be data + details from the database TBD  </p>
+
+<h1>Cost of Living</h1>
+<p>   This will be data + details from the database TBD  </p>
+
+</summary>
+
 `,
   styleUrl: './details.component.css'
 })
