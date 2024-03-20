d441b6ba576a1d920b1f47813592e67aab5befa8
Daniel Pathammavong <patha037@umn.edu>
2024-03-20 14:26:01 -0500

    moved quick flame element down and added cost of living element

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,27 +3,46 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [FlexLayoutModule],
   template: `
-  <article>
-  <h2 class="listing-heading">{{housingLocation?.name}}</h2>
-    <img class="listing-photo" [src]="housingLocation?.photo"
+  <article fxLayout= "column">
+      <section fxLayout= "column" fxLayoutAlign="right" >
+</section>
+
+  <h2 class="item item-1" class="listing-heading">{{housingLocation?.name}}</h2>
+    <img class="item item-2" class="listing-photo" [src]="housingLocation?.photo"
       alt="Exterior photo of {{housingLocation?.name}}"/>
     <section class="listing-description">
-      <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      <p fxLayoutAlign="center center"class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
     </section>
-    <section class="listing-features">
-      <h2 class="section-heading">Quick Flame</h2>
-      <ul>
-        <li>Average Cost of Living: {{housingLocation?.availableUnits}}</li>
-        <li>Healthcare Quality: {{housingLocation?.wifi}}</li>
-        <li>Population: {{housingLocation?.laundry}}</li>
+    <section class="item item-3 listing-features" fxFlex="10%" >
+      <div>
+      <h2 fxLayoutAlign="center center" class="section-heading">Quick Flame</h2>
+      </div>
+      <div> 
+      <ul fxLayout= "row" fxLayoutAlign="space-between">
+        <li class="item item-1" >Average Cost of Living: {{housingLocation?.availableUnits}}</li>
+        <li class="item item-2">Healthcare Quality: {{housingLocation?.wifi}}</li>
+        <li class="item item-3">Population: {{housingLocation?.laundry}}</li>
       </ul>
-    </section>
+</div>
+
+ </section>
+
+
+
+ <section>
+  <h2> Cost of Living </h2>
+  <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+</section>
+
+
+    
   </article>
 `,
   styleUrl: './details.component.css'
