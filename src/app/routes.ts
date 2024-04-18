ff0477f5d405e0377dade6ce57e3a0e2f7159b7f
mandi andrade <mandi.andrade20@gmail.com>
2024-04-18 12:51:52 -0500

    more edits to style the header to wireframe and added about route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { AboutComponent } from './about/about.component';
 
 const routeConfig: Routes = [
     {
@@ -12,6 +13,11 @@ const routeConfig: Routes = [
       path: 'details/:id',
       component: DetailsComponent,
       title: 'Location details'
+    },
+    {
+      path: 'about',
+      component: AboutComponent,
+      title: 'About FIRE'
     }
   ];
   
